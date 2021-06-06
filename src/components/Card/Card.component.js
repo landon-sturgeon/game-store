2cf5e12c62558ef53be3483e7d57d035694be18c
sturgeon.landon <sturgeon.landon@gmail.com>
2021-06-05 22:26:01 -0800

    added cardlist styling and multiple cards

--- src/components/Card/Card.component.js
+++ src/components/Card/Card.component.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import { Container, CardSide, Content, CardForm } from "./Card.styles";
 
 class Card extends Component {
   render() {
     return (
-      <div class="card">
-        <img src={this.props.img} />
-        <h1>{this.props.title}</h1>
-        <p>{this.props.description}</p>
-      </div>
+      <Container>
+        <CardSide front>
+          <Content>
+            <h1>{this.props.title}</h1>
+            <p>{this.props.description}</p>
+          </Content>
+        </CardSide>
+        <CardSide>
+          <Content>
+            <h1>Contact Me</h1>
+            <CardForm>
+              <label>Your Name: </label>
+              <input type="text" placeholder="Enter Name Here" />
+              <label>Your E-mail: </label>
+              <input type="text" placeholder="Enter E-mail Here" />
+              <label>Your message: </label>
+              <textarea placeholder="Enter message here"></textarea>
+              <input type="submit" value="Done" />
+            </CardForm>
+          </Content>
+        </CardSide>
+      </Container>
     );
   }
 }
--- src/components/CardList/CardList.component.js
+++ src/components/CardList/CardList.component.js
@@ -1,36 +1,23 @@
 import React, { Component } from "react";
-import { Container, CardSide, Content, CardForm } from "./CardList.styles";
+import { CardListContainer } from "./CardList.styles";
+import Card from "../Card/Card.component";
 
+const cards = [
+  { title: "Test Card 1", description: "this is a description of card 1" },
+  { title: "Test Card 2", description: "this is a description of card 2" },
+  { title: "Test Card 3", description: "this is a description of card 3" },
+  { title: "Test Card 4", description: "this is a description of card 4" },
+  { title: "Test Card 5", description: "this is a description of card 5" },
+  { title: "Test Card 6", description: "this is a description of card 6" },
+];
 class CardList extends Component {
   render() {
     return (
-      <Container>
-        <CardSide front>
-          <Content>
-            <h1>Landon Sturgeon</h1>
-            <p>
-              I am a graphic designer and art director. I am as well specialised
-              in front end web design, user experience and creating identities.
-              Throughout my career, I have worked with companies of all shapes
-              and sizes that enriched my experience
-            </p>
-          </Content>
-        </CardSide>
-        <CardSide>
-          <Content>
-            <h1>Contact Me</h1>
-            <CardForm>
-              <label>Your Name :</label>
-              <input type="text" placeholder="Enter Name Here" />
-              <label>Your E-mail :</label>
-              <input type="text" placeholder="Example@mail.com" />
-              <label>Your message :</label>
-              <textarea placeholder="The Subject"></textarea>
-              <input type="submit" value="Done" />
-            </CardForm>
-          </Content>
-        </CardSide>
-      </Container>
+      <CardListContainer>
+        {cards.map((card, i) => (
+          <Card key={i} title={card.title} description={card.description} />
+        ))}
+      </CardListContainer>
     );
   }
 }
